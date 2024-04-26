ec81fde5d9a44575674763dca580faf90bca0661
linskin <1360293790@qq.com>
2024-04-26 17:03:37 +0800

    去除无用文件

---
+++ build_executable.py
@@ -0,0 +1,10 @@
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    'main.py',
+    '--onefile',  # 打包成一个单独的可执行文件
+    '--noconsole',  # 不显示控制台窗口（对于GUI应用程序）
+    '--icon=i12.ico'
+])
+# 打包用
+# python build_executable.py
--- 打包.py
+++
@@ -1,9 +0,0 @@
-import PyInstaller.__main__
-
-PyInstaller.__main__.run([
-    '4.py',
-    '--onefile',  # 打包成一个单独的可执行文件
-    '--noconsole',  # 不显示控制台窗口（对于GUI应用程序）
-    '--icon=i11.ico'
-])
-# pyinstaller 4.py --onefile --noconsole --icon=i12.ico
